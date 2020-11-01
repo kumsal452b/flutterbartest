f7a6ec384bdfd530c734c750c63cf27cb5a67287
kumsal65 <yahya.alatas.mdbf17@iste.edu.tr>
2020-11-01 22:15:54 +0300

    add to bottom bar

--- lib/widget/mybottombar.dart
+++ lib/widget/mybottombar.dart
@@ -1,30 +1,116 @@
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
 
 class myAppBottombr extends StatefulWidget {
+  final int selectedTab;
+  final Function(int) tabClic;
+  const myAppBottombr({Key key, this.selectedTab,this.tabClic}) : super(key: key);
+
   @override
   _State createState() => _State();
 }
 
 class _State extends State<myAppBottombr> {
-  
+  int selectTab=2;
+  @override
+  void initState() {
+
+    super.initState();
+  }
   @override
   Widget build(BuildContext context) {
     return Container(
+      padding: EdgeInsets.symmetric(
+        horizontal: 4,
+        vertical: 10
+      ),
+      decoration: BoxDecoration(
+        color: Colors.white,
+        borderRadius: BorderRadius.circular(20),
+        boxShadow: [
+          BoxShadow(
+              color:Colors.black.withOpacity(0.48),
+              spreadRadius: 1,
+              blurRadius: 15
+          )
+        ]
+      ),
       child: Row(
+
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
         children: [
-          BottomBar()
+          BottomBar(
+            path: 'assets/images/tab_home.png',
+            isSelect: selectTab==0?true:false,
+            clickItem: (){
+              setState(() {
+                selectTab=0;
+              });
+            },
+          ),
+          BottomBar(
+            path: 'assets/images/tab_saved.png',
+            isSelect: selectTab==1?true:false,
+            clickItem: (){
+              setState(() {
+                selectTab=1;
+              });
+            },
+          ),
+          BottomBar(
+            path: 'assets/images/tab_logout.png',
+            isSelect: selectTab==2?true:false,
+            clickItem: (){
+              setState(() {
+                selectTab=2;
+              });
+            },
+          ),
+          BottomBar(
+            path: 'assets/images/tab_search.png',
+            isSelect: selectTab==3?true:false,
+            clickItem: (){
+              setState(() {
+                selectTab=3;
+              });
+            },
+          ),
         ],
       ),
     );
   }
 }
 class BottomBar extends StatelessWidget {
+  final String path;
+  final bool isSelect;
+  final Function clickItem;
+  const BottomBar({Key key, this.path,this.isSelect, this.clickItem}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: Image(
-        image: AssetImage(
-          'assets/images/tab_home.png'
+    bool selected=isSelect??false;
+    return GestureDetector(
+      onTap: clickItem,
+      child: Container(
+        decoration: BoxDecoration(
+          border: Border(
+            top: BorderSide(
+              color: selected?Colors.redAccent:Colors.transparent,
+              width: 2,
+
+            )
+          )
+        ),
+        padding: EdgeInsets.symmetric(
+          vertical: 8
+        ),
+        child: Image(
+          height: 35,
+          width: 35,
+          color: selected?Colors.redAccent:Colors.black,
+          image: AssetImage(
+            path
+          ),
         ),
       ),
     );
