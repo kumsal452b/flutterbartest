edc106ba5c04c785dc970160eca00ff7d2cf09f7
kumsal65 <yahya.alatas.mdbf17@iste.edu.tr>
2020-11-01 19:58:50 +0300

    test is bottom bar

--- lib/widget/mybottombar.dart
+++ lib/widget/mybottombar.dart
@@ -10,14 +10,24 @@ class _State extends State<myAppBottombr> {
   @override
   Widget build(BuildContext context) {
     return Container(
-      
+      child: Row(
+        children: [
+          BottomBar()
+        ],
+      ),
     );
   }
 }
-class  extends StatelessWidget {
+class BottomBar extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Container(
+      child: Image(
+        image: AssetImage(
+          'assets/images/tab_home.png'
+        ),
+      ),
+    );
   }
 }
 
