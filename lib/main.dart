188676e3a2ebd0596e74e357546649a59260beb2
kumsal65 <yahya.alatas.mdbf17@iste.edu.tr>
2020-11-03 21:56:00 +0300

    Test is sillt

--- lib/main.dart
+++ lib/main.dart
@@ -11,7 +11,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+      home: MyHomePage(title: 'F Page'),
     );
   }
 }
